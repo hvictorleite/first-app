de7b26d4e342dc983f3b44d347e1874a38d47e40
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-07-17 15:59:01 -0300

    Using ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,7 +8,15 @@ export class ServerComponent {
   serverId = 10;
   serverStatus = 'offline';
 
-  getServerStatus() {
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus(): string {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
