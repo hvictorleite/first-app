918c28acf5b65b73c24ccfa58e52d726de504d70
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-07-25 15:52:07 -0300

    Understanding the function and calling order of Angular hooks -> ngOnDestroy

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements DoCheck, AfterContentChecked, AfterViewInit, AfterViewChecked {
+export class ServerElementComponent implements OnDestroy {
   @Input('srvElement') element: { type: string, name: string, content: string };
   @Input() name: string;
 
@@ -22,19 +22,23 @@ export class ServerElementComponent implements DoCheck, AfterContentChecked, Aft
   //   console.log('ngOnInit called!');
   // }
 
-  ngDoCheck(): void {
-    console.log('ngDoCheck called!');
-  }
+  // ngDoCheck(): void {
+  //   console.log('ngDoCheck called!');
+  // }
 
-  ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked called!');
-  }
+  // ngAfterContentChecked(): void {
+  //   console.log('ngAfterContentChecked called!');
+  // }
 
-  ngAfterViewInit(): void {
-    console.log('ngAfterViewInit called!');
-  }
+  // ngAfterViewInit(): void {
+  //   console.log('ngAfterViewInit called!');
+  // }
 
-  ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked called!');
+  // ngAfterViewChecked(): void {
+  //   console.log('ngAfterViewChecked called!');
+  // }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called!');
   }
 }
