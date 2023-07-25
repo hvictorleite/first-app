53883765d644a8d2956141baa078e05772b0200b
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-07-25 14:26:42 -0300

    Understanding the function and calling order of Angular hooks -> ngOnChanges and ngOnInit

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
 
-  constructor() {}
+  constructor() {
+    console.log('constructor called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
 
   ngOnInit() {
-
+    console.log('ngOnInit called!');
   }
 }
