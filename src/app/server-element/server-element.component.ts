ce4111e37dc13086e3551d03aafb60d679a1079a
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-07-25 15:31:30 -0300

    Understanding the function and calling order of Angular hooks -> ngDoCheck and ngAfterContentChecked

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements DoCheck, AfterContentChecked {
   @Input('srvElement') element: { type: string, name: string, content: string };
   @Input() name: string;
 
-  constructor() {
-    console.log('constructor called!');
+  // constructor() {
+  //   console.log('constructor called!');
+  // }
+
+  // ngOnChanges(changes: SimpleChanges) {
+  //   console.log('ngOnChanges called!');
+  //   console.log(changes);
+  // }
+
+  // ngOnInit() {
+  //   console.log('ngOnInit called!');
+  // }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    console.log('ngOnChanges called!');
-    console.log(changes);
-  }
-
-  ngOnInit() {
-    console.log('ngOnInit called!');
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!');
   }
 }
