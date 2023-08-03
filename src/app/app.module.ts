cdbad6337a43b6341ab5f7cb37e2b83020a8c62b
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-03 12:07:04 -0300

    Services module started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
 
-import { BasicHighLightDirective } from './basic-highlight/basic-highlight.directive';
-import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
-import { UnlessDirective } from './unless/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighLightDirective,
-    BetterHighlightDirective,
-    UnlessDirective
+    AccountComponent,
+    NewAccountComponent
   ],
   imports: [
     BrowserModule,
