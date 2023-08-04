97d889c4f9f7b1b24650e0a0e973943f3d5071fb
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-04 15:52:09 -0300

    Using the highest possible level of Hierarchical Injector to provide a service to every project, including other services.

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
   constructor(private accountsService: AccountsService, private loggingService: LoggingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
