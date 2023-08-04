7ef86121005d2bec633310cc565aabbb33b546dd
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-04 13:55:19 -0300

    AccountsService created. Refactoring the code to use it (accounts property still does not update view)

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,25 +1,18 @@
+import { AccountsService } from '../accounts.service';
 import { LoggingService } from './../logging.service';
-import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [AccountsService, LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
-
-  private loggingService?: LoggingService;
-  constructor() {
-    this.loggingService = inject(LoggingService);
-  }
+  constructor(private accountsService: AccountsService, private loggingService: LoggingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountsService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus);
   }
 }
