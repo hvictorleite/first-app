d8346f0ca89e8f0555f25c43710b679a290942ad
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-15 16:27:49 -0300

    Section 'Changing Pages with Routing' started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [UsersService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
