62b8ac85d5f49a5598bbda04b680ecb25e2941bc
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-25 15:27:22 -0300

    Using resolve guard to send data dynamically before the route is called and the component is initialized.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+// import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
@@ -10,11 +10,12 @@ import { UsersComponent } from "./users/users.component";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent },
+    { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
     { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
   ] },
   { path: 'users', component: UsersComponent, children: [
@@ -33,4 +34,4 @@ const appRoutes: Routes = [
 })
 export class AppRoutingModule {
 
-}
+5}
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Subscription } from 'rxjs';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ServersService } from '../servers.service';
 
@@ -8,29 +8,32 @@ import { ServersService } from '../servers.service';
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit, OnDestroy {
+export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
-  paramsSubscription: Subscription;
+  // paramsSubscription: Subscription;
 
-  constructor(private serversService: ServersService,
-              private router: Router,
+  constructor(private router: Router,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const idServer: number = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(idServer);
+    this.route.data.subscribe(
+      (data: Data) => { this.server = data['server']; }
+    );
 
-    this.paramsSubscription = this.route.params
-      .subscribe((params: Params) => {
-        this.server = this.serversService.getServer(+params['id']);
-      });
+    // const idServer: number = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(idServer);
+
+    // this.paramsSubscription = this.route.params
+    //   .subscribe((params: Params) => {
+    //     this.server = this.serversService.getServer(+params['id']);
+    //   });
   }
 
   onEdit(): void {
     this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
   }
 
-  ngOnDestroy(): void {
-    this.paramsSubscription.unsubscribe();
-  }
+  // ngOnDestroy(): void {
+  //   this.paramsSubscription.unsubscribe();
+  // }
 }
