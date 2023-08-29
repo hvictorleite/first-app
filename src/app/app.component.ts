07b0939a1e1a57d6b148ff1d32844febb2938c9f
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-29 14:53:56 -0300

    Subjects.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  activatedSub: Subscription;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe({
+      next: (data) => this.userActivated = data
+    })
+  }
+
+  ngOnDestroy() {
+    this.activatedSub.unsubscribe();
+  }
+}
