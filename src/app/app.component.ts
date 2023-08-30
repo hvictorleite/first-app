4f280350cfa3f55db92ee769db85f636f014c0b1
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-08-30 10:05:57 -0300

    Forms in Angular module starts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,12 @@
-import { Subscription } from 'rxjs';
-import { UserService } from './user.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated = false;
-  activatedSub: Subscription;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.activatedSub = this.userService.activatedEmitter.subscribe({
-      next: (data) => this.userActivated = data
-    })
-  }
-
-  ngOnDestroy() {
-    this.activatedSub.unsubscribe();
+export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
-import { AppRoutingModule } from './app-routing.module';
 
-import { UserService } from './user.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
-  providers: [UserService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
