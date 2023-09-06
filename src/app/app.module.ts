59ddcd3648028be9af66e05956245e50e3d2ccbe
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-09-06 13:05:01 -0300

    Creating a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
+import { ShortenPipe } from './shorten.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
