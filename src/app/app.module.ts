2162bdb82ab80e3764676c963954ced08dc4fbf6
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-09-06 16:57:49 -0300

    Reverse pipe created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
+import { ReversePipe } from './reverse.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReversePipe
   ],
   imports: [
     BrowserModule,
