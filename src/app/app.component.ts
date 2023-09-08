dc38ea037aba6e57b0022f0cc6b8066179cc93cb
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-09-08 15:45:22 -0300

    GETting data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -25,9 +27,19 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('https://ng-complete-guide-v-default-rtdb.firebaseio.com/posts.json')
+      .subscribe({
+        next: (posts) => {
+          console.log(posts);
+        }
+      });
   }
 }
