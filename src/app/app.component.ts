f64153f3c4108df6b8bf55c60e94d7ee44cb2909
Victor Hugo Marques Rosa Leite <vhleite@firjan.com.br>
2023-09-11 14:46:35 -0300

    Outputting posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Post } from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -48,8 +48,7 @@ export class AppComponent implements OnInit {
       }))
       .subscribe({
         next: (posts) => {
-          // ...
-          console.log(posts);
+          this.loadedPosts = posts;
         }
       });
   }
